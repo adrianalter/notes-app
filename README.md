6c9fc7d95ed147eee2f80a8728e4522ef42d9555
Adrian <adrianlungu1995@gmail.com>
2024-05-27 10:47:53 +0200

    started React stuff

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ python manage.py startapp api
 # Create user
 touch api/serializers.py
 in views.py
+
+
+# Create frontend
+Go to root folder
+npm create vite@latest frontend -- --template react
+cd frontend
+npm install axios react-react-dom jwt-decode
+
+Go to frontend/src and create folders "components, pages, styles"
+Also create api.js and constants.js
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,41 @@
+import {Navigate} from "react-router-dom"
+import {jwtDecode} from "jwt-decode"
+import api from "../api"
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
+import {useState} from "react"
+
+function ProtectedRoute({children}) {
+    const [isAuthorized, setIsAuthorize] = useState(null)
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try{
+            const res = await api.post("/api/token/refresh/", {refresh: refreshToken})
+        } catch (error) {
+            console.log(error)
+            setIsAuthorize(false)
+        }
+    }
+
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if (!token) {
+            setIsAuthorize(false)
+            return
+        }
+        const decoded = jwtDecode(token)
+        const tokenExpiration = decoded.exp
+        const now = Date.now() / 1000
+        if (tokenExpiration < now){
+            await refreshToken()
+        } else{
+            setIsAuthorize(true)
+        }
+    }
+
+    if(isAuthorized == null) {
+        return <div>Loading...</div>
+    }
+
+    return isAuthorized ? children : <Navigate to="login"/>
+}
